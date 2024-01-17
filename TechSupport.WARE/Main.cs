ac85e2d79b6dc4b12d897b824c669d5d39b48961
Kristerwh <111729904+Kristerwh@users.noreply.github.com>
2024-01-17 14:24:01 +0100

    Oppdaterring

--- TechSupport.WARE/Main.cs
+++ TechSupport.WARE/Main.cs
@@ -9,9 +9,14 @@ namespace TechSupport.WARE
 {
     internal class Main
     {
-        Package bigPackages = new Package();
-        bigPackages.add(1249346934, 400000, 400, 500, 200);
+        static void Main(string[] args)
+        {
+            Package myPackages = new Package();
+            myPackages.add(1249346934, 400000, 400, 500, 200);
 
+            Warehouse myWarehouse = new Warehouse;
+            myWarehouse.addIsle(1, 50,)
+        }
 
 
     }
