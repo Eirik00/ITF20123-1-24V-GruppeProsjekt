07781c4dce4af4780c7264a58a65a1d8fac4de64
Kristerwh <111729904+Kristerwh@users.noreply.github.com>
2024-01-17 14:05:48 +0100

    Update Main.cs

--- TechSupport.WARE/Main.cs
+++ TechSupport.WARE/Main.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,10 @@ namespace TechSupport.WARE
 {
     internal class Main
     {
+        Package bigPackages = new Package();
+        bigPackages.add(1249346934, 400000, 400, 500, 200);
+
+
+
     }
 }
