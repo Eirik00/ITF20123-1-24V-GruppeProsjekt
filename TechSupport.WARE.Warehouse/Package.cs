fcf39829efe56d7d02f662cf07d133e8dbc6d630
Kristerwh <111729904+Kristerwh@users.noreply.github.com>
2024-01-22 17:45:08 +0100

    Package update, status

--- TechSupport.WARE.Warehouse/Package.cs
+++ TechSupport.WARE.Warehouse/Package.cs
@@ -7,27 +7,34 @@ using System.Threading.Tasks;
 
 namespace TechSupport.WARE
 {
+    public enum statusList { Reception = 1, Storage = 2, InProgress = 3, Delivery = 4 };
     public class Package
     {
         private int packageId;
         private int packageLenghtInMm;
         private int packageHeightInMm;
         private int packageDepthInMm;
-        private int packageWeighInMm;
+        private int packageWeighInGrams;
         private bool isFragile;
         private int storageSpecification;
-        private int status;
+        private statusList status;
 
-        public Package(int packageId, int packageLenghtInMm, int packageHeightInMm, int packageDepthInMm, int packageWeighInMm, bool isFragile, int storageSpecification, int status = 0)
+        public Package(int packageId, int packageLenghtInMm, int packageHeightInMm, int packageDepthInMm, int packageWeighInMm, bool isFragile, int storageSpecification, statusList status = statusList.Reception)
         {
             this.packageId = packageId;
             this.packageLenghtInMm = packageLenghtInMm;
             this.packageHeightInMm = packageHeightInMm;
             this.packageDepthInMm = packageDepthInMm;
-            this.packageWeighInMm = packageWeighInMm;
+            this.packageWeighInGrams = packageWeighInMm;
             this.isFragile = isFragile;
             this.storageSpecification = storageSpecification;
             this.status = status;
         }
+
+        public string getStatus(Package package)
+        {
+            return this.status.ToString;
+        }
+
     }
 }
