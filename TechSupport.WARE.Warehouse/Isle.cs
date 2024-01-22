c20fabd4f6f65d71896d6e39378b1c952f09212f
Kristerwh <111729904+Kristerwh@users.noreply.github.com>
2024-01-22 15:44:53 +0100

    Isle update

--- TechSupport.WARE.Warehouse/Isle.cs
+++ TechSupport.WARE.Warehouse/Isle.cs
@@ -8,12 +8,34 @@ namespace TechSupport.WARE
 {
     public class Isle : IIsle
     {
-        public Isle(int isleid)
+        private int numberOfSpaces;
+        private int lengthOfSpaceInMm;
+        private int heightOfSpaceInMm;
+        private int depthOfSpaceInMm;
+        private int weightLimitInGrams;
+        private int category;
+
+        public Isle(int numberOfSpaces, int lengthOfSpaceInMm, int heightOfSpaceInMm, int depthOfSpaceInMm, int weightLimitInGrams, int category, int isleId)
         {
-            isleId = isleid;
+            this.numberOfSpaces = numberOfSpaces;
+            this.lengthOfSpaceInMm = lengthOfSpaceInMm;
+            this.heightOfSpaceInMm = heightOfSpaceInMm;
+            this.depthOfSpaceInMm = depthOfSpaceInMm;
+            this.weightLimitInGrams = weightLimitInGrams;
+            this.category = category;
+            this.isleId = isleId;
         }
 
         public int isleId { get; set; }
 
+        public void addPackage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removePackage()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
