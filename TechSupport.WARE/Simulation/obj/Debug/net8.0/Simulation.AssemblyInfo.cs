f50119e11d91d2b7e0cdcca654053921c926af8d
Eirik00 <eirik.rene@gmail.com>
2024-01-22 16:55:41 +0100

    Gitignore

--- TechSupport.WARE/Simulation/obj/Debug/net8.0/Simulation.AssemblyInfo.cs
+++ TechSupport.WARE/Simulation/obj/Debug/net8.0/Simulation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b1fed0c8070efd40253c1cbb780b051f67785e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
