eceb2eaaa2f0965f6cc2b79a04003693ebd61a10
MartinInformatikk <martinmalo2003@gmail.com>
2024-01-26 18:30:39 +0100

    Made interface and class for PackagesList

---
+++ TechSupport.WARE.Warehouse/IPackagesList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.WARE.Warehouse
+{
+    internal interface IPackagesList
+    {
+        public void addPackageToList(Package package);
+    }
+}
---
+++ TechSupport.WARE.Warehouse/PackagesList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.WARE.Warehouse
+{
+    public class PackagesList : IPackagesList
+    {
+        private int listId;
+        private ArrayList packages;
+
+        public PackagesList(int listId, ArrayList packages)
+        {
+            this.listId = listId;
+            this.packages = packages;
+
+        }
+        public void addPackageToList(Package package)
+        {
+            this.packages.Add(package);
+        }
+    }
+}
