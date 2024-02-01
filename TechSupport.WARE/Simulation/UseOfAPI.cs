73604698a7d41a498988c1534437b7e87e8c16f0
Kristerwh <111729904+Kristerwh@users.noreply.github.com>
2024-02-01 10:42:54 +0100

    Created UseOfAPI.cs

---
+++ TechSupport.WARE/Simulation/UseOfAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation
+{
+    internal class UseOfAPI
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
