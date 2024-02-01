7144e15a3e42e741464a28559932d0ba8b5728c9
Kristerwh <111729904+Kristerwh@users.noreply.github.com>
2024-02-01 10:30:27 +0100

    IContacts XML

--- TechSupport.WARE.Warehouse/Contact/IContact.cs
+++ TechSupport.WARE.Warehouse/Contact/IContact.cs
@@ -8,12 +8,39 @@ namespace TechSupport.WARE.Warehouse
 {
     internal interface IContact
     {
+        /// <summary>
+        /// Get the first name of Contact.
+        /// </summary>
         string FirstName { get; }
+
+        /// <summary>
+        /// Get the surname of Contact.
+        /// </summary>
         string Surname { get; }
+
+        /// <summary>
+        /// Get the Contact's email.
+        /// </summary>
         string Email { get; }
+
+        /// <summary>
+        /// Get the Contact's country.
+        /// </summary>
         string Country { get; }
+
+        /// <summary>
+        /// Get the phone number of the Contact.
+        /// </summary>
         int PhoneNumber { get; }
+
+        /// <summary>
+        /// Get the Contact's address.
+        /// </summary>
         string Address { get; }
+
+        /// <summary>
+        /// Get the Contact's postal code.
+        /// </summary>
         int PostalCode { get; }
     }
 }
