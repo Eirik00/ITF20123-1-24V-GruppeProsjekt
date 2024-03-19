d494716c0ea9e942a3b3102e756146a9c3c23605
MartinInformatikk <martinmalo2003@gmail.com>
2024-03-19 17:54:42 +0100

    Viktig å huske å lagre :)

--- TechSupport.WARE.Warehouse/Employee/Employee.cs
+++ TechSupport.WARE.Warehouse/Employee/Employee.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TechSupport.WARE.Warehouse.Exceptions;
 
-namespace TechSupport.WARE.Warehouse.Employee
+namespace TechSupport.WARE.Warehouse
 {
     public class Employee : Contact
     {
