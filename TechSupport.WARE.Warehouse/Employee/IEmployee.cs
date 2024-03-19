07dfaf6cb56a72a8e2c2745c10d0fe67ec2a8b80
MartinInformatikk <martinmalo2003@gmail.com>
2024-03-19 18:10:12 +0100

    Placeholder interface for Employee class

---
+++ TechSupport.WARE.Warehouse/Employee/IEmployee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.WARE.Warehouse
+{
+    internal interface IEmployee
+    {
+        //Empty as of now, don't know if Employee class should have its own interface, considering it's a child of Contact class. 
+    }
+}
