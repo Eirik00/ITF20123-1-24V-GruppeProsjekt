77b413414bcf9f3ff2be627ed5efbcfdff7ac71e
Eirik00 <eirik.rene@gmail.com>
2024-04-22 15:45:00 +0200

    Create Aisle

--- TechSupport.WARE/TechSupport.WARE.GUI/CreateAisle.xaml.cs
+++ TechSupport.WARE/TechSupport.WARE.GUI/CreateAisle.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TechSupport.WARE.Warehouse;
 
 namespace TechSupport.WARE.GUI
 {
@@ -19,9 +21,44 @@ namespace TechSupport.WARE.GUI
     /// </summary>
     public partial class CreateAisle : Window
     {
-        public CreateAisle()
+        private MainWindow _mainWindow;
+        public CreateAisle(MainWindow mainWindow)
         {
             InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+
+        private void ForceTextToNumber(object sender, TextCompositionEventArgs e)
+        {
+            Regex regx = new Regex("[^0-9]+");
+            e.Handled = regx.IsMatch(e.Text);
+        }
+
+        private void createAisleFunction(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int shelves = Int32.Parse(aisleShevles.Text);
+                int places = Int32.Parse(aislePlacesPerShelf.Text);
+                int weight = Int32.Parse(aisleWeight.Text);
+                int length = Int32.Parse(aisleLegnth.Text);
+                int height = Int32.Parse(aisleHeight.Text);
+                int depth = Int32.Parse(aisleDepth.Text);
+                int aisleId = _mainWindow.GetNextAvailableAisleId();
+
+                Aisle newAisle = new(shelves, places, length, height, depth, weight, aisleId);
+                _mainWindow.AddAisletoList(aisleName.Text, newAisle);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void SendEventToMain(object sender, EventArgs e)
+        {
+            _mainWindow.RefreshAisleList();
         }
     }
 }
--- TechSupport.WARE/TechSupport.WARE.GUI/MainWindow.xaml.cs
+++ TechSupport.WARE/TechSupport.WARE.GUI/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TechSupport.WARE.Warehouse;
 
 namespace TechSupport.WARE.GUI
 {
@@ -16,15 +20,42 @@ namespace TechSupport.WARE.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<String, Aisle> _aisleList = new Dictionary<String, Aisle>();
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        public void RefreshAisleList()
+        {
+            lstAisle.Items.Clear();
+            foreach (KeyValuePair<String, Aisle> aisle in _aisleList)
+                lstAisle.Items.Add(aisle.Key);
+        }
+
         private void OpenCreateAisleButton(object sender, RoutedEventArgs e)
         {
-            CreateAisle createAisleWindow = new CreateAisle();
+            CreateAisle createAisleWindow = new CreateAisle(this);
             createAisleWindow.ShowDialog();
+        }
+        public void AddAisletoList(String key, Aisle aisle)
+        {
+            _aisleList.Add(key, aisle);
+        }
+        public int GetNextAvailableAisleId()
+        {
+            return _aisleList.Count;
+        }
+
+        private void AisleListSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Get the clicked ListBoxItem
+            var item = ItemsControl.ContainerFromElement(sender as ListBox, e.OriginalSource as DependencyObject) as ListBoxItem;
+            if (item != null)
+            {
+                Aisle selectedAisle = _aisleList[item.Content.ToString()];
+                curAisleName.Content = "Name: " + item.Content.ToString();
+            }
         }
     }
 }
